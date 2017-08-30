6a5161b1dde36bce018d9633badf0285a5f57b24
arju88nair <arju88nair@gmail.com>
2017-08-30 20:58:49 +0530

    Initialisation

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Home;
+use View;
+use Auth;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Redirect;
+
+class HomeController extends Controller
+{
+    //
+
+
+
+
+}
---
+++ app/Models/Home.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nair
+ * Date: 21/8/17
+ * Time: 5:11 PM
+ */
+
+namespace App\Models;
+
+use DB;
+use Illuminate\Support\Facades\Mail;
+use Redirect;
+use View;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Database\Eloquent\Model;
+use Route;
+
+define('API_URL', 'http://srv1.brandclub.mobi:8082/');
+session_start();
+
+class Home extends Model
+{
+
+
+
+
+}
+
+
